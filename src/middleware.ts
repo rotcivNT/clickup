127ca89e0e76c4e8e605b447d5f605a31ddc3e59
Ngọc Thắng <ngocthang.devweb@gmail.com>
2023-12-20 01:14:48 +0700

    Init Project

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: [],
+  afterAuth: (auth, req, res) => {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  }
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
