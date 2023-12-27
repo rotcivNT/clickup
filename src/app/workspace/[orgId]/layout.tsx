d3aec78030fbb57f54a57bfcc759aca4c0910cb9
Ngọc Thắng <ngocthang.devweb@gmail.com>
2023-12-27 13:20:22 +0700

    revert best layout

--- src/app/workspace/[orgId]/layout.tsx
+++ src/app/workspace/[orgId]/layout.tsx
@@ -4,7 +4,6 @@ import MainTopBar from "@/components/workspace/components/dashboard-main/MainTop
 import DashboardSidebar from "@/components/workspace/components/dashboard-sidebar/DashboardSidebar";
 import { cn } from "@/lib/utils";
 import { Be_Vietnam_Pro } from "next/font/google";
-import Link from "next/link";
 
 export const BeVietnamese = Be_Vietnam_Pro({
   weight: ["300", "400", "500", "600", "700"],
@@ -15,32 +14,9 @@ export default function WorkspaceLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const data = [
-    {
-      title: "Home",
-    },
-    {
-      title: "Inbox",
-    },
-    {
-      title: "Docs",
-    },
-    {
-      title: "Dashboard",
-    },
-    {
-      title: "Whiteboard",
-    },
-    {
-      title: "Pulse",
-    },
-    {
-      title: "Goal",
-    },
-  ];
   return (
     <div className={BeVietnamese.className}>
-      {/* <DashboardHeader />
+      <DashboardHeader />
       <main className={cn("flex")}>
         <DashboardSidebar />
         <div className="flex-1 flex flex-col bg-[#2A2E34] relative">
@@ -48,16 +24,7 @@ export default function WorkspaceLayout({
           <DashboardMain>{children}</DashboardMain>
           <div className="bg-work-linear" />
         </div>
-      </main> */}
-      <DashboardHeader />
-      <div>
-        {data.map((item) => (
-          <Link href={`/workspace/org_2a0Clf7H1fpJ61KA8k3l7tKYWcO/${item.title.toLowerCase()}`}>
-            {item.title}
-          </Link>
-        ))}
-      </div>
-      {children}
+      </main>
     </div>
   );
 }
